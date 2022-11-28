8d359aa869bc66514bc12097cf1da85366e59375
odiabliuk <o.diabliuk@gmail.com>
2022-11-28 13:17:22 +0200

    Move loading logic to separeta file

--- src/pages/Info.js
+++ src/pages/Info.js
@@ -10,39 +10,7 @@ import {
 } from "firebase/firestore";
 import CardSkeleton from "../components/CardSkeleton";
 import Iterator from "../components/Iterator";
-
-class LoadingState  {
-    constructor() {
-        this.state = new Loading();
-    }
-
-    nextState() {
-        this.state = this.state.next();
-    }
-}
-
-class LoadingStatus {
-    constructor(name, nextStatus) {
-        this.name = name;
-        this.nextStatus = nextStatus;
-    }
-
-    next() {
-        return new this.nextStatus();
-    }
-}
-
-class Loading extends LoadingStatus {
-    constructor() {
-        super('loading', Ready)
-    }
-}
-
-class Ready extends LoadingStatus {
-    constructor() {
-        super('ready', Ready)
-    }
-}
+import LoadingState from "../components/LoadingState";
 
 const Info = () => {
     const [newName, setNewName] = useState("");
